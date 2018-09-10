92220005e37e5e9cc33a9ec57f48f61958628b79
VadymBoguslavsky <axixe92@gmail.com>
2018-09-10 12:56:29 +0300

    slider

--- js/script.js
+++ js/script.js
@@ -30,7 +30,6 @@ function myFunction() {
       maxBtn = $('.max'),
       range_min = $('.range_min'),
       range_max = $('.range_max'),
-      some = $('.range__from'),
       minVal = parseInt($(minBtn).val()),
       maxVal = parseInt($(maxBtn).val()),
       origin = $(this).context.className;
@@ -40,12 +39,14 @@ function myFunction() {
     }
     var minVal = parseInt($(minBtn).val());
     $(range_min).html("От " + addSeperator(minVal * 1));
-
+    $('#range__from').text("От " + addSeperator(minVal * 1))
     if (origin === 'max' && maxVal - 5 < minVal) {
       $(maxBtn).val(1 + minVal);
     }
     var maxVal = parseInt($(maxBtn).val());
     $(range_max).html("- до " + addSeperator(maxVal * 1));
+    $('#range__to').text("- до " + addSeperator(maxVal * 1))
+
   }
 
   $('input[type="range"]').on('input', rangeInputChangeEventHandler);
@@ -53,3 +54,75 @@ function myFunction() {
 
 
 
+var x, i, j, selElmnt, a, b, c;
+/*look for any elements with the class "custom-select":*/
+x = document.getElementsByClassName("custom-select");
+for (i = 0; i < x.length; i++) {
+  selElmnt = x[i].getElementsByTagName("select")[0];
+  /*for each element, create a new DIV that will act as the selected item:*/
+  a = document.createElement("DIV");
+  a.setAttribute("class", "select-selected");
+  a.innerHTML = selElmnt.options[selElmnt.selectedIndex].innerHTML;
+  x[i].appendChild(a);
+  /*for each element, create a new DIV that will contain the option list:*/
+  b = document.createElement("DIV");
+  b.setAttribute("class", "select-items select-hide");
+  for (j = 0; j < selElmnt.length; j++) {
+    /*for each option in the original select element,
+    create a new DIV that will act as an option item:*/
+    c = document.createElement("DIV");
+    c.innerHTML = selElmnt.options[j].innerHTML;
+    c.addEventListener("click", function(e) {
+        /*when an item is clicked, update the original select box,
+        and the selected item:*/
+        var y, i, k, s, h;
+        s = this.parentNode.parentNode.getElementsByTagName("select")[0];
+        h = this.parentNode.previousSibling;
+        for (i = 0; i < s.length; i++) {
+          if (s.options[i].innerHTML == this.innerHTML) {
+            s.selectedIndex = i;
+            h.innerHTML = this.innerHTML;
+            y = this.parentNode.getElementsByClassName("same-as-selected");
+            for (k = 0; k < y.length; k++) {
+              y[k].removeAttribute("class");
+            }
+            this.setAttribute("class", "same-as-selected");
+            break;
+          }
+        }
+        h.click();
+    });
+    b.appendChild(c);
+  }
+  x[i].appendChild(b);
+  a.addEventListener("click", function(e) {
+      /*when the select box is clicked, close any other select boxes,
+      and open/close the current select box:*/
+      e.stopPropagation();
+      closeAllSelect(this);
+      this.nextSibling.classList.toggle("select-hide");
+      this.classList.toggle("select-arrow-active");
+    });
+}
+function closeAllSelect(elmnt) {
+  /*a function that will close all select boxes in the document,
+  except the current select box:*/
+  var x, y, i, arrNo = [];
+  x = document.getElementsByClassName("select-items");
+  y = document.getElementsByClassName("select-selected");
+  for (i = 0; i < y.length; i++) {
+    if (elmnt == y[i]) {
+      arrNo.push(i)
+    } else {
+      y[i].classList.remove("select-arrow-active");
+    }
+  }
+  for (i = 0; i < x.length; i++) {
+    if (arrNo.indexOf(i)) {
+      x[i].classList.add("select-hide");
+    }
+  }
+}
+/*if the user clicks anywhere outside the select box,
+then close all select boxes:*/
+document.addEventListener("click", closeAllSelect);
